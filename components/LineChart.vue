c33dc874b7997d871e7d0187ab95327d92f75acf
Felix Korch <korch.felix@gmail.com>
2018-11-25 16:42:11 +0100

    init

---
+++ components/LineChart.vue
@@ -0,0 +1,12 @@
+<script>
+  import { Line } from 'vue-chartjs'
+ 
+  export default {
+    name: 'line-chart',
+    extends: Line,
+    props: ['data', 'options'],
+    mounted () {
+      this.renderChart(this.data, this.options)
+    }
+  }
+</script>
